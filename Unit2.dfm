ef457020564b21b62df339b41710f259fbf6eb7c
ReSerb <rat1999@list.ru>
2021-04-21 11:48:04 +0700

    7.1.2

--- Unit2.dfm
+++ Unit2.dfm
@@ -2,8 +2,8 @@ object Form2: TForm2
   Left = 0
   Top = 0
   Caption = 'Form2'
-  ClientHeight = 299
-  ClientWidth = 635
+  ClientHeight = 198
+  ClientWidth = 495
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -13,4 +13,29 @@ object Form2: TForm2
   OldCreateOrder = False
   PixelsPerInch = 96
   TextHeight = 13
+  object Button1: TButton
+    Left = 8
+    Top = 8
+    Width = 105
+    Height = 49
+    Caption = 'Button1'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+  object Memo1: TMemo
+    Left = 119
+    Top = 8
+    Width = 370
+    Height = 185
+    TabOrder = 1
+  end
+  object Button2: TButton
+    Left = 8
+    Top = 144
+    Width = 105
+    Height = 49
+    Caption = 'Button2'
+    TabOrder = 2
+    OnClick = Button2Click
+  end
 end
--- Unit2.pas
+++ Unit2.pas
@@ -4,14 +4,20 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
 
 type
   TForm2 = class(TForm)
+    Button1: TButton;
+    Memo1: TMemo;
+    Button2: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
   private
     { Private declarations }
   public
-    { Public declarations }
+   procedure KvadrKoren(a,b,c:real; var x1,x2:real);
+   function Faktorial(n:word):word;
   end;
 
 var
@@ -20,5 +26,43 @@ var
 implementation
 
 {$R *.dfm}
+
+{ TForm2 }
+
+procedure TForm2.Button1Click(Sender: TObject);
+var a1,b1,c1, x1,x2:real;
+begin
+a1:=1; b1:=2; c1:=-3;
+x1:=10000; x2:=10000;
+InputBox('', 'начальные значения', 'x1= '+FloatToStr(x1)+' x2= '+FloatToStr(x2));
+KvadrKoren(1, 2, -3, x1,x2);
+InputBox('Процедура Квадр', 'корни', 'x1= '+FloatToStr(x1)+' x2= '+FloatToStr(x2));
+end;
+
+procedure TForm2.Button2Click(Sender: TObject);
+var i:integer; b:word;
+begin
+for i:=1 to 10 do
+Memo1.Lines.Add('Факториал n='+IntToStr(i)+' равен '+IntToStr(Faktorial(i)));
+b:=Faktorial(10);
+ShowMessage('10! ='+IntToStr(b));
+end;
+
+function TForm2.Faktorial(n: word): word;
+var i,j:word;
+begin
+j:=1;
+for i:=1 to n do j:=j*i;
+Result:=j;
+end;
+
+procedure TForm2.KvadrKoren(a, b, c: real; var x1, x2: real);
+var D:real;
+begin
+D:=sqr(b)-4*a*c;
+if D<0 then begin ShowMessage('Корней нет');exit; end;
+x1:=(-b+sqrt(D))/(2*a); x2:=(-b-sqrt(D))/(2*a);
+a:=10000; b:=10000; c:=10000;
+end;
 
 end.
